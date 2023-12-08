ad61cd88411a081f31dee1ca6c136a6c2b361e60
desoo <marcus60.2001@gmail.com>
2023-12-08 20:05:40 +0300

    Employee book done

---
+++ src/Employee.java
@@ -0,0 +1,55 @@
+public class Employee {
+    private String fullName;
+    private int department;
+    private int salary;
+    private int id;
+
+    private static int counter = 0;
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        if (department < 0 && department > 5) {
+            throw new IllegalArgumentException("Значение департамента должно быть от 1 до 5");
+        }
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Значение зарплаты должно быть положительным.");
+        }
+        this.salary = salary;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ФИО: " + fullName +
+                ". Отдел: " + department +
+                ". Зарплата: " + salary +
+                " рублей. ID: " + id;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,211 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee one = new Employee("Мичурин Александр Вячеславович", 1, 80_000);
+        Employee two = new Employee("Козлов Максим Андреевич", 5, 60_000);
+        Employee three = new Employee("Буев Дмитрий Игоревич", 4, 70_000);
+        Employee four = new Employee("Шеременьтев Игорь Александрович", 3, 100_000);
+        Employee five = new Employee("Дубровский Виктор Дмитриевич", 2, 40_000);
+        Employee six = new Employee("Леонов Никита Андреевич", 5, 90_000);
+        Employee seven = new Employee("Невский Вячеслав Олегович", 1, 85_000);
+        Employee eight = new Employee("Бажанов Артем Дмитриевич", 1, 82_000);
+        Employee nine = new Employee("Войнолович Юрий Владимирович", 4, 65_000);
+        Employee ten = new Employee("Дубровский Владимир Анатольевич", 2, 60_000);
+
+
+        Employee[] employee = new Employee[10];
+        employee[0] = one;
+        employee[1] = two;
+        employee[2] = three;
+        employee[3] = four;
+        employee[4] = five;
+        employee[5] = six;
+        employee[6] = seven;
+        employee[7] = eight;
+        employee[8] = nine;
+        employee[9] = ten;
+
+        printInfo(employee);
+        printSalary(employee);
+        printMinSalary(employee);
+        printMaxSalary(employee);
+        printAverageSalary(employee);
+        printFullName(employee);
+        indexingSalary(employee);
+        printMinDepartment(1, employee);
+        printMaxDepartment(1, employee);
+        printTotalDepartment(1, employee);
+        indexingDepartment(1,employee);
+        printAllEmployees(1,employee);
+        printMinNumber(67000, employee);
+        printMaxNumber(67000, employee);
+    }
+
+    public static void printInfo(Employee[] employee) {
+        System.out.println("Базовая сложность:\na)");
+        System.out.println("Список сотрудников со всеми данными:");
+        for (Employee value : employee) {
+            System.out.println(value);
+        }
+    }
+
+    public static void printSalary(Employee[] employee) {
+        System.out.println("b)");
+        int sum = 0;
+        for (Employee value : employee) {
+            sum += value.getSalary();
+        }
+        System.out.println("Общая сумма затрат на зарплату в месяц = " + sum + " рублей.");
+    }
+
+    public static void printMinSalary(Employee[] employee) {
+        System.out.println("c)");
+        int min = Integer.MAX_VALUE;
+        String name = "";
+        for (Employee value : employee) {
+            if (value.getSalary() < min) {
+                min =value.getSalary();
+                name = value.getFullName();
+            }
+        }
+        System.out.println("Сотрудник с минимальной зарплатой в " + min + " рублей. Это " + name);
+    }
+
+    public static void printMaxSalary(Employee[] employee) {
+        System.out.println("d)");
+        int min = Integer.MIN_VALUE;
+        String name = "";
+        for (Employee value : employee) {
+            if (value.getSalary() > min) {
+                min =value.getSalary();
+                name = value.getFullName();
+            }
+        }
+        System.out.println("Сотрудник с максимальной зарплатой в " + min + " рублей. Это " + name);
+    }
+
+    public static void printAverageSalary(Employee[] employee) {
+        System.out.println("e)");
+        int sum = 0;
+        int total = employee.length;
+        int average = 0;
+        for (Employee value : employee) {
+            sum += value.getSalary();
+            average = sum / total;
+        }
+        System.out.println("Среднее значение зарплат = " + average + " рублей.");
+    }
+
+    public static void printFullName(Employee[] employee) {
+        System.out.println("f)");
+        System.out.println("Ф.И.О. сотрудников");
+        for (Employee value : employee) {
+            System.out.println(value.getFullName());
+        }
+    }
+
+    public static void indexingSalary(Employee[] employee) {
+        System.out.println("Повышенная сложность:\n1)");
+        int sum = 0;
+        int index = 2;
+        String name = "";
+        int total = 0;
+        for (int i = 0; i < employee.length; i++) {
+            sum = employee[i].getSalary();
+            name = employee[i].getFullName();
+            total = total + sum;
+            total = total + (total * index / 100);
+            System.out.println("Зарплата сотрудника: " + name + ". При индексации зарплаты в 2% составляет: "
+                    + total + " рублей.");
+        }
+    }
+
+    public static void printMinDepartment(int department, Employee[] employee) {
+        System.out.println("2. a)");
+        int min = Integer.MAX_VALUE;
+        String name = "";
+        for (Employee value : employee) {
+            if (value.getSalary() < min && department == value.getDepartment()) {
+                min =value.getSalary();
+                name = value.getFullName();
+            }
+        }
+        System.out.println("Сотрудник с минимальной зарплатой в " + min + " рублей. В отделе №1, это " + name);
+    }
+
+    public static void printMaxDepartment(int department, Employee[] employee) {
+        System.out.println("2. b)");
+        int min = Integer.MIN_VALUE;
+        String name = "";
+        for (Employee value : employee) {
+            if (value.getSalary() > min && department == value.getDepartment()) {
+                min =value.getSalary();
+                name = value.getFullName();
+            }
+        }
+        System.out.println("Сотрудник с максимальной зарплатой в " + min + " рублей. В отделе № 1, это " + name);
+    }
+
+    public static void printTotalDepartment(int department, Employee[] employee) {
+        System.out.println("2. c)");
+        int sum = 0;
+        for (Employee value : employee) {
+            if (department == value.getDepartment()) {
+                sum += value.getSalary();
+            }
+        }
+        System.out.println("Сумма затрат на зарплату в отделе №1, составляет = "
+                + sum + " рублей.");
+    }
+
+    public static void indexingDepartment(int department, Employee[] employee) {
+        System.out.println("2. e)");
+        int sum = 0, index = 2, total = 0;
+        String name = "";
+        for (Employee value : employee) {
+            if (department == value.getDepartment()) {
+                sum = value.getSalary();
+                name = value.getFullName();
+                total = sum + (sum * index / 100);
+                System.out.println("Зарплата сотрудника: " + name + ". При индексации зарплаты в 2% составляет: "
+                        + total + " рублей.");
+            }
+        }
+    }
+
+    public static void printAllEmployees(int department, Employee[] employee) {
+        System.out.println("2. f)");
+        for (Employee value : employee) {
+            if (department == value.getDepartment()) {
+                System.out.println("ID: " + value.getId() + ". Ф.И.О. " + value.getFullName()
+                        + ". Зарплата: " + value.getSalary() + " рублей.");
+            }
+        }
+
+    }
+
+    public static void printMinNumber(int number, Employee[] employee) {
+        System.out.println("3. a)");
+        int sum = 0;
+        String name = "";
+        int id = 0;
+        for (Employee value : employee) {
+            sum = value.getSalary();
+            id = value.getId();
+            name = value.getFullName();
+            if (number > sum) System.out.println("ID: " + id + ". Ф.И.О. " + name + ". Зарплата: " + sum);
+        }
+    }
+
+    public static void printMaxNumber(int number, Employee[] employee) {
+        System.out.println("3. b)");
+        int sum = 0, id = 0;
+        String name = "";
+        for (Employee value : employee) {
+            sum = value.getSalary();
+            id = value.getId();
+            name = value.getFullName();
+            if (sum >= number) System.out.println("ID: " + id + ". Ф.И.О. " + name + ". Зарплата: " + sum);
+        }
     }
 }
